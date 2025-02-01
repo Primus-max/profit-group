9e41f5d179db17b363dfde7d6aa1dab5a26ee0a9
Katerina <79203161+Primus-max@users.noreply.github.com>
2025-02-01 13:32:02 +0300

    Рефикторинг advantages

--- src/components/sections/OurAdvantages.vue
+++ src/components/sections/OurAdvantages.vue
@@ -1,10 +1,15 @@
 <template>
     <BaseSection background="var(--color-white)" height="152vh" :z-index="6" has-radius>
-            <div class="advantages_container">
-                <div class="advantages_header">
-                    <h2 class="advantages_title"> НАШИ ПРЕИМУЩЕСТВА</h2>
-                </div>
-                <div class="legal_services_conteiner">
+        <div class="advantages_container">
+            <div class="advantages_header">
+                <h2 class="advantages_title"> НАШИ ПРЕИМУЩЕСТВА</h2>
+            </div>
+            <div v-for="advanatage in infos" :key="id">
+                <Advantage :info="advanatage">
+                    <FolderIcon :color="'white'" />
+                </Advantage>
+            </div>
+            <!-- <div class="legal_services_conteiner">
                     <div class="advantages-info_icon"> <FolderIcon :color="'white'"/></div>
                     <div class="legal_services_content">
                         <span class="advantages-info_title">ПОЛНЫЙ СПЕКТР ЮРИДИЧЕСКИХ УСЛУГ</span>
@@ -48,9 +53,9 @@
                         <p class="advantages-info__description">Мы верим в полную прозрачность: Вы не столкнетесь с
                             какими-либо неожиданными сборами</p>
                     </div>
-                </div>
-            </div>
-      
+                </div> -->
+        </div>
+
     </BaseSection>
 </template>
 
@@ -61,6 +66,22 @@ import Gavelicon from '@/components/icons/GavelIcon.vue'
 import Scalesicon from '@/components/icons/ScalesIcon.vue'
 import Shieldicon from '@/components/icons/ShieldIcon.vue'
 import StarCrownicon from '@/components/icons/StarCrownIcon.vue'
+import Advantage from '@/components/Advantage.vue'
+
+
+const infos = [
+    {
+        id: 0,
+        title: 'ПОЛНЫЙ СПЕКТР ЮРИДИЧЕСКИХ УСЛУГ',
+        description: "Мы предлагаем комплексные юридические услуги, специализируясь на бизнесе, недвижимости и автомобильном праве",
+        icon: () =>  FolderIcon.vue
+    },
+    {
+        id: 1,
+        title: 'ПОДТВЕРЖДЕННЫЙ ПОСЛУЖНОЙ СПИСОК',
+        description: "Мы обеспечиваем клиентам значительные возмещения в спорах, связанных с недвижимостью и страхованием"
+    },
+]
 </script>
 
 
@@ -87,7 +108,7 @@ import StarCrownicon from '@/components/icons/StarCrownIcon.vue'
     color: var(--color-black)
 }
 
-.legal_services_conteiner {
+.conteiner {
     width: 100%;
     height: 190px;
     margin-block: 10px;
@@ -95,16 +116,14 @@ import StarCrownicon from '@/components/icons/StarCrownIcon.vue'
     flex-direction: row-reverse;
     background-color: var(--color-black);
     border-radius: 20px;
-
-
 }
 
-.legal_services_content{
+.content {
     width: 80%;
     height: 100%;
     display: flex;
     flex-direction: column;
-   
+
 }
 
 .track_record_conteiner {
@@ -117,7 +136,7 @@ import StarCrownicon from '@/components/icons/StarCrownIcon.vue'
     border-radius: 20px;
 }
 
-.track_record_content{
+.track_record_content {
     width: 80%;
     height: 100%;
     display: flex;
@@ -134,7 +153,7 @@ import StarCrownicon from '@/components/icons/StarCrownIcon.vue'
     border-radius: 20px;
 }
 
-.specialized_expertise_content{
+.specialized_expertise_content {
     width: 80%;
     height: 100%;
     display: flex;
@@ -151,7 +170,7 @@ import StarCrownicon from '@/components/icons/StarCrownIcon.vue'
     border-radius: 20px;
 }
 
-.customer-oriented_approach_content{
+.customer-oriented_approach_content {
     width: 80%;
     height: 100%;
     display: flex;
@@ -168,14 +187,14 @@ import StarCrownicon from '@/components/icons/StarCrownIcon.vue'
     border-radius: 20px;
 }
 
-.pricing_content{
+.pricing_content {
     width: 80%;
     height: 100%;
     display: flex;
     flex-direction: column;
 }
 
-.advantages-info_title {
+.title {
     width: 100%;
     height: 100%;
     margin-left: 20px;
@@ -187,7 +206,7 @@ import StarCrownicon from '@/components/icons/StarCrownIcon.vue'
     flex-direction: column;
 }
 
-.advantages-info__description {
+.description {
     width: 90%;
     height: 100%;
     margin-left: 20px;
@@ -197,11 +216,12 @@ import StarCrownicon from '@/components/icons/StarCrownIcon.vue'
     font-size: 0.9rem;
     line-height: 1.3rem;
 }
-.advantages-info_icon{
+
+.icon {
     width: 60px;
     height: 60px;
-    color:white;
-    border-radius:40%;
+    color: white;
+    border-radius: 40%;
     margin-top: 5px;
     margin-right: 10px;
     display: flex;
@@ -210,18 +230,17 @@ import StarCrownicon from '@/components/icons/StarCrownIcon.vue'
 
 
 /* для таблетки */
-@media (min-width: 600px) and ( max-width:1024px) 
-{
+@media (min-width: 600px) and (max-width:1024px) {
     .advantages_container {
-    width: 100%;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    color: white;
-}
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        color: white;
+    }
 
-.advantages_header {
-    width: 100%;
+    .advantages_header {
+        width: 100%;
         height: 100px;
         margin-top: 30px;
         display: flex;
@@ -232,97 +251,95 @@ import StarCrownicon from '@/components/icons/StarCrownIcon.vue'
         color: var(--color-black);
         align-items: center;
         justify-content: space-around;
-}
+    }
 
-.legal_services_conteiner {
-    width: 100%;
-    height: 235px;
-    margin-block: 10px;
-    display: flex;
-    flex-direction: row-reverse;
-    background-color: var(--color-black);
-    border-radius: 20px;
+    .legal_services_conteiner {
+        width: 100%;
+        height: 235px;
+        margin-block: 10px;
+        display: flex;
+        flex-direction: row-reverse;
+        background-color: var(--color-black);
+        border-radius: 20px;
+    }
 
+    .legal_services_content {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
 
-}
+    }
 
-.legal_services_content{
-    width: 100%;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-   
-}
+    .track_record_conteiner {
+        width: 100%;
+        height: 235px;
+        margin-block: 10px;
+        display: flex;
+        flex-direction: row-reverse;
+        background-color: var(--color-blue);
+        border-radius: 20px;
+    }
 
-.track_record_conteiner {
-    width: 100%;
-    height: 235px;
-    margin-block: 10px;
-    display: flex;
-    flex-direction: row-reverse;
-    background-color: var(--color-blue);
-    border-radius: 20px;
-}
+    .track_record_content {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+    }
 
-.track_record_content{
-    width: 100%;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-}
+    .specialized_expertise_conteiner {
+        width: 100%;
+        height: 235px;
+        margin-block: 10px;
+        display: flex;
+        flex-direction: row-reverse;
+        background-color: var(--color-black);
+        border-radius: 20px;
+    }
 
-.specialized_expertise_conteiner {
-    width: 100%;
-    height: 235px;
-    margin-block: 10px;
-    display: flex;
-    flex-direction: row-reverse;
-    background-color: var(--color-black);
-    border-radius: 20px;
-}
+    .specialized_expertise_content {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+    }
 
-.specialized_expertise_content{
-    width: 100%;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-}
+    .customer-oriented_approach_conteiner {
+        width: 100%;
+        height: 235px;
+        margin-block: 10px;
+        display: flex;
+        background-color: var(--color-blue);
+        flex-direction: row-reverse;
+        border-radius: 20px;
+    }
 
-.customer-oriented_approach_conteiner {
-    width: 100%;
-    height: 235px;
-    margin-block: 10px;
-    display: flex;
-    background-color: var(--color-blue);
-    flex-direction: row-reverse;
-    border-radius: 20px;
-}
+    .customer-oriented_approach_content {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+    }
 
-.customer-oriented_approach_content{
-    width: 100%;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-}
+    .pricing_conteiner {
+        width: 100%;
+        height: 235px;
+        margin-block: 10px;
+        display: flex;
+        background-color: var(--color-black);
+        flex-direction: row-reverse;
+        border-radius: 20px;
+    }
 
-.pricing_conteiner {
-    width: 100%;
-    height: 235px;
-    margin-block: 10px;
-    display: flex;
-    background-color: var(--color-black);
-    flex-direction: row-reverse;
-    border-radius: 20px;
-}
+    .pricing_content {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+    }
 
-.pricing_content{
-    width: 100%;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-}
-
-.advantages-info_title {
+    .advantages-info_title {
         width: 90%;
         height: 50%;
         margin-bottom: 15px;
@@ -331,33 +348,33 @@ import StarCrownicon from '@/components/icons/StarCrownIcon.vue'
         font-weight: var(--fw-medium);
         text-align: left;
         flex-direction: column;
-}
+    }
 
-.advantages-info__description {
-    width: 90%;
-    height: 100%;
-    margin-left: 20px;
-    margin-bottom: 20px;
-    font-weight: var(--fw-light);
-    text-align: left;
-    font-size: 1.3rem;
-    line-height: 1.3rem;
-}
-.advantages-info_icon{
-    width: 80px;
-    height: 80px;
-    color:white;
-    border-radius:40%;
-    margin-top: 10px;
-    margin-right: 20px;
-    display: flex;
-}
+    .advantages-info__description {
+        width: 90%;
+        height: 100%;
+        margin-left: 20px;
+        margin-bottom: 20px;
+        font-weight: var(--fw-light);
+        text-align: left;
+        font-size: 1.3rem;
+        line-height: 1.3rem;
+    }
+
+    .advantages-info_icon {
+        width: 80px;
+        height: 80px;
+        color: white;
+        border-radius: 40%;
+        margin-top: 10px;
+        margin-right: 20px;
+        display: flex;
+    }
 
 }
 
 /* настройки для десктопа */
-@media (min-width: 1024px) 
-{
+@media (min-width: 1024px) {
     .advantages_container {
         width: 100%;
         height: 100%;
@@ -365,10 +382,10 @@ import StarCrownicon from '@/components/icons/StarCrownIcon.vue'
         flex-direction: row;
         color: white;
         flex-wrap: wrap;
-}
+    }
 
-.advantages_header {
-         width: 100%;
+    .advantages_header {
+        width: 100%;
         height: 100px;
         margin-top: 30px;
         display: flex;
@@ -379,102 +396,100 @@ import StarCrownicon from '@/components/icons/StarCrownIcon.vue'
         color: var(--color-black);
         align-items: center;
         /* justify-content: flex-start; */
-}
+    }
 
-.legal_services_conteiner {
-    width: 45%;
-    height: 235px;
-    margin-inline: 0px 15px;
-    display: flex;
-    flex-direction: row-reverse;
-    background-color: var(--color-black);
-    border-radius: 20px;
+    .legal_services_conteiner {
+        width: 45%;
+        height: 235px;
+        margin-inline: 0px 15px;
+        display: flex;
+        flex-direction: row-reverse;
+        background-color: var(--color-black);
+        border-radius: 20px;
+    }
 
+    .legal_services_content {
+        width: 100%;
+        height: 100%;
+        margin-top: 10px;
+        display: flex;
+        flex-direction: column;
 
-}
+    }
 
-.legal_services_content{
-    width: 100%;
-    height: 100%;
-    margin-top: 10px;
-    display: flex;
-    flex-direction: column;
-   
-}
+    .track_record_conteiner {
+        width: 45%;
+        height: 235px;
+        margin-inline: 0px 15px;
+        display: flex;
+        flex-direction: row-reverse;
+        background-color: var(--color-blue);
+        border-radius: 20px;
+    }
 
-.track_record_conteiner {
-    width: 45%;
-    height: 235px;
-    margin-inline: 0px 15px;
-    display: flex;
-    flex-direction: row-reverse;
-    background-color: var(--color-blue);
-    border-radius: 20px;
-}
+    .track_record_content {
+        width: 100%;
+        height: 100%;
+        margin-top: 10px;
+        display: flex;
+        flex-direction: column;
+    }
 
-.track_record_content{
-    width: 100%;
-    height: 100%;
-    margin-top: 10px;
-    display: flex;
-    flex-direction: column;
-}
+    .specialized_expertise_conteiner {
+        width: 45%;
+        height: 235px;
+        margin-inline: 0px 15px;
+        display: flex;
+        flex-direction: row-reverse;
+        background-color: var(--color-blue);
+        border-radius: 20px;
+    }
 
-.specialized_expertise_conteiner {
-    width: 45%;
-    height: 235px;
-    margin-inline: 0px 15px;
-    display: flex;
-    flex-direction: row-reverse;
-    background-color: var(--color-blue);
-    border-radius: 20px;
-}
+    .specialized_expertise_content {
+        width: 100%;
+        height: 100%;
+        margin-top: 10px;
+        display: flex;
+        flex-direction: column;
+    }
 
-.specialized_expertise_content{
-    width: 100%;
-    height: 100%;
-    margin-top: 10px;
-    display: flex;
-    flex-direction: column;
-}
+    .customer-oriented_approach_conteiner {
+        width: 45%;
+        height: 235px;
+        margin-inline: 0px 15px;
+        display: flex;
+        background-color: var(--color-black);
+        flex-direction: row-reverse;
+        border-radius: 20px;
+    }
 
-.customer-oriented_approach_conteiner {
-    width: 45%;
-    height: 235px;
-    margin-inline: 0px 15px;
-    display: flex;
-    background-color: var(--color-black);
-    flex-direction: row-reverse;
-    border-radius: 20px;
-}
+    .customer-oriented_approach_content {
+        width: 100%;
+        height: 100%;
+        margin-top: 10px;
+        display: flex;
+        flex-direction: column;
+    }
 
-.customer-oriented_approach_content{
-    width: 100%;
-    height: 100%;
-    margin-top: 10px;
-    display: flex;
-    flex-direction: column;
-}
+    .pricing_conteiner {
+        width: 45%;
+        height: 235px;
+        margin-inline: 0px 15px;
+        display: flex;
+        background-color: var(--color-black);
+        flex-direction: row-reverse;
+        border-radius: 20px;
+    }
 
-.pricing_conteiner {
-    width: 45%;
-    height: 235px;
-    margin-inline: 0px 15px;
-    display: flex;
-    background-color: var(--color-black);
-    flex-direction: row-reverse;
-    border-radius: 20px;
-}
+    .pricing_content {
+        width: 100%;
+        height: 100%;
+        margin-top: 10px;
+        display: flex;
+        flex-direction: column;
+    }
 
-.pricing_content{
-    width: 100%;
-    height: 100%;
-    margin-top: 10px;
-    display: flex;
-    flex-direction: column;
-}
-
-.advantages-info_title {
+    .advantages-info_title {
         width: 90%;
         height: 50%;
         margin-bottom: 15px;
@@ -483,26 +498,27 @@ import StarCrownicon from '@/components/icons/StarCrownIcon.vue'
         font-weight: var(--fw-light);
         text-align: left;
         flex-direction: column;
-}
+    }
 
-.advantages-info__description {
-    width: 90%;
-    height: 100%;
-    margin-left: 20px;
-    margin-bottom: 20px;
-    font-weight: var(--fw-light);
-    text-align: left;
-    font-size: 1.1rem;
-    line-height: 1.3rem;
-}
-.advantages-info_icon{
-    width: 80px;
-    height: 80px;
-    color:white;
-    border-radius:40%;
-    margin-top: 18px;
-    margin-right: 20px;
-    display: flex;
-}
+    .advantages-info__description {
+        width: 90%;
+        height: 100%;
+        margin-left: 20px;
+        margin-bottom: 20px;
+        font-weight: var(--fw-light);
+        text-align: left;
+        font-size: 1.1rem;
+        line-height: 1.3rem;
+    }
+
+    .advantages-info_icon {
+        width: 80px;
+        height: 80px;
+        color: white;
+        border-radius: 40%;
+        margin-top: 18px;
+        margin-right: 20px;
+        display: flex;
+    }
 }
 </style>
